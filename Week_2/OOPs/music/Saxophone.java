423f007e181c2e13a825cfcefacc4fa7951852c5
Priyanshu Singh <priyanshusingh192004@gmail.com>
2026-06-14 22:10:34 +0530

    OOps

---
+++ Week_2/OOPs/music/Saxophone.java
@@ -0,0 +1,9 @@
+package Week_2.OOPs.music;
+
+public class Saxophone implements Playable {
+
+    public void play() {
+        System.out.println("Playing Saxophone");
+    }
+}
+
---
+++ Week_2/OOPs/music/playable.java
@@ -0,0 +1,4 @@
+package Week_2.OOPs.music;
+interface Playable{
+    void play();
+}
